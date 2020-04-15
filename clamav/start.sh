5bbfdcd04b147d7abce8f8538338d8febb9e1c15
Kyoungho An <kyoungho.an@gmail.com>
2020-04-15 23:25:58 +0000

    updated clamav

---
+++ clamav/start.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env /bin/sh
+[ -f /data/main.cvd ] || /app/freshclam --config-file=/app/conf/freshclam.conf
+/app/freshclam -d -c 6 --config-file=/app/conf/freshclam.conf
+sleep infinity
+echo "Update is completed. Ready to scan"
