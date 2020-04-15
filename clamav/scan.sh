2594c41d54c75afe9e076851b8d62c58ebe6660c
Kyoungho An <kyoungho.an@gmail.com>
2020-04-15 23:54:49 +0000

    updated clamav configs

--- clamav/scan.sh
+++ clamav/scan.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env /bin/sh
 # Perform a scan, but only if one isn't already in progress
+if [ -z "$1"]
+  then
+    SCAN_DIR="/host-fs/opt"
+else
+    SCAN_DIR=$1
+fi
+
+echo "Start Scanning..." $SCAN_DIR
+
 LOCK="/tmp/clamscan.lock"
 
 if [ -f "$LOCK" ];then
@@ -15,6 +24,6 @@ else
     --log=/logs/clamscan.log \
     --recursive \
     --database=/data \
-    /host-fs/opt
+    $SCAN_DIR
   rm $LOCK
 fi
--- clamav/start.sh
+++ clamav/start.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env /bin/sh
 [ -f /data/main.cvd ] || /app/freshclam --config-file=/app/conf/freshclam.conf
 /app/freshclam -d -c 6 --config-file=/app/conf/freshclam.conf
-sleep infinity
 echo "Update is completed. Ready to scan"
+sleep infinity
+
